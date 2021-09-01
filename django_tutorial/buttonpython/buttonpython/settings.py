58a8f751ad9e654c4430eb008d2a503cb2c519c1
aubzie305 <aubsg99@gmail.com>
2021-09-01 12:07:53 -0500

    Add bootstrap SUCCESS

--- django_tutorial/buttonpython/buttonpython/settings.py
+++ django_tutorial/buttonpython/buttonpython/settings.py
@@ -116,8 +116,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+#-----------------------------------------------------
+STATIC_ROOT = BASE_DIR / 'root'
+#-----------------------------------------------------
+STATICFILES_DIRS = [
+      BASE_DIR / 'static',
+      BASE_DIR / 'bootstrap',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
